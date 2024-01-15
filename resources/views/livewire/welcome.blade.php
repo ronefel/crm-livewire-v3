5ef96c26744742ad9d129f1c5b864ab007ca4d90
ronefel <ronefel@hotmail.com>
2024-01-15 14:13:32 -0400

    instalado tailwind css

--- resources/views/livewire/welcome.blade.php
+++ resources/views/livewire/welcome.blade.php
@@ -1,3 +1,3 @@
-<div>
+<div class="text-red-400 bg-red-100 rounded m-20">
     welcome
 </div>
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+import forms from '@tailwindcss/forms'
+import typo from '@tailwindcss/typography'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    forms, typo
+  ],
+}
+
