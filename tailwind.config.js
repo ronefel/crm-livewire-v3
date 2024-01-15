06b0722f1bfb79a7a54bc9dbc612c37f14675885
ronefel <ronefel@hotmail.com>
2024-01-15 15:11:06 -0400

    instalado mary-ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,15 @@
-import forms from '@tailwindcss/forms'
-import typo from '@tailwindcss/typography'
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
+    "./resources/**/**/*.blade.php",
+    "./resources/**/**/*.js",
+    "./app/View/Components/**/**/*.php",
+    "./app/Livewire/**/**/*.php",
+    "./vendor/robsontenorio/mary/src/View/Components/**/*.php"
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    forms, typo
-  ],
+  plugins: [require("daisyui")],
 }
 
