adc4ca1051b51e205468777d5844aec23a9f2d97
ronefel <ronefel@hotmail.com>
2024-01-20 12:01:42 -0400

    frontend filtrando pelas permissões

--- app/Livewire/Admin/Users/Index.php
+++ app/Livewire/Admin/Users/Index.php
@@ -3,11 +3,16 @@
 namespace App\Livewire\Admin\Users;
 
 use App\Enums\Can;
-use App\Models\User;
+use App\Models\{Permission, User};
+use Illuminate\Database\Eloquent\{Builder, Collection};
 use Illuminate\Support\Facades\Log;
 use Livewire\Attributes\Computed;
 use Livewire\{Component, WithPagination};
 
+/**
+ * @property-read Collection|User[] $users
+ * @property-read array $headers
+ */
 class Index extends Component
 {
     use WithPagination;
@@ -16,9 +21,13 @@ class Index extends Component
 
     public array $search_permissions = [];
 
+    public Collection $permissionsToSearch;
+
     public function mount()
     {
         $this->authorize(Can::BE_AN_ADMIN->value);
+        // $this->filterPermissions();
+        $this->getPermissions();
     }
     public function render()
     {
@@ -55,5 +64,18 @@ class Index extends Component
             ['key' => 'email', 'label' => 'Email', 'sortColumnBy' => 'id', 'sortDirection' => 'asc'],
             ['key' => 'permissions.key', 'label' => 'Permissions', 'sortColumnBy' => 'id', 'sortDirection' => 'asc'],
         ];
+    }
+
+    // public function filterPermissions(?string $value = null)
+    // {
+    //     $this->permissionsToSearch = Permission::query()->when($value, function (Builder $query) use ($value) {
+    //         $query->where('key', 'like', '%' . $value . '%');
+    //     })->orderBy('key')->get();
+
+    // }
+
+    private function getPermissions()
+    {
+        $this->permissionsToSearch = Permission::query()->orderBy('key')->get();
     }
 }
--- resources/views/livewire/admin/users/index.blade.php
+++ resources/views/livewire/admin/users/index.blade.php
@@ -1,10 +1,12 @@
 <div>
-    <x-header title="Users" separator progress-indicator>
+    <x-header title="Users" separator progress-indicator="gotoPage,search,search_permissions">
         <x-slot:middle class="!justify-end">
             <x-input icon="o-magnifying-glass" class="input-sm" placeholder="Search..." wire:model.lazy="search" />
+            <x-choices-offline class="select-sm" placeholder="Filter by Permissions"
+                wire:model.live="search_permissions" :options="$permissionsToSearch" option-label="key" searchable />
         </x-slot:middle>
     </x-header>
-    <x-table :headers="$this->headers" :rows="$this->users" with-pagination>
+    <x-table :headers=" $this->headers" :rows="$this->users" with-pagination>
         @scope('cell_permissions.key', $user)
         @foreach ($user->permissions as $permission)
         <x-badge :value="$permission->key" class="badge-primary" />
