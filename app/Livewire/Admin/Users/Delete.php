04f578d33719ba286a1154c83dc659c382eab953
ronefel <ronefel@hotmail.com>
2024-01-22 15:16:59 -0400

    confirmando antes de deletar o usuário

--- app/Livewire/Admin/Users/Delete.php
+++ app/Livewire/Admin/Users/Delete.php
@@ -3,11 +3,17 @@
 namespace App\Livewire\Admin\Users;
 
 use App\Models\User;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 class Delete extends Component
 {
     public User $user;
+
+    #[Rule(['required', 'confirmed'])]
+    public string $confirmation = 'DELETAR';
+
+    public ?string $confirmation_confirmation = null;
 
     public function render()
     {
@@ -16,7 +22,10 @@ class Delete extends Component
 
     public function destroy()
     {
+        $this->validate();
+
         $this->user->delete();
+
         $this->dispatch('user::deleted');
     }
 }
--- tests/Feature/Admin/UserManagement/DeleteUserTest.php
+++ tests/Feature/Admin/UserManagement/DeleteUserTest.php
@@ -4,15 +4,30 @@ use App\Livewire\Admin\Users\Delete;
 use App\Models\User;
 use Livewire\Livewire;
 
-use function Pest\Laravel\assertSoftDeleted;
+use function Pest\Laravel\{actingAs, assertNotSoftDeleted, assertSoftDeleted};
 
 it('deve deletar um usuário', function () {
     $user        = User::factory()->admin()->create();
     $forDeletion = User::factory()->create();
 
+    actingAs($user);
     Livewire::test(Delete::class, ['user' => $forDeletion])
+        ->set('confirmation_confirmation', 'DELETAR')
         ->call('destroy')
         ->assertDispatched('user::deleted');
 
     assertSoftDeleted('users', ['id' => $forDeletion->id]);
+});
+
+it('deve confirmar antes de deletar o usuário', function () {
+    $user        = User::factory()->admin()->create();
+    $forDeletion = User::factory()->create();
+
+    actingAs($user);
+    Livewire::test(Delete::class, ['user' => $forDeletion])
+        ->call('destroy')
+        ->assertHasErrors(['confirmation' => 'confirmed'])
+        ->assertNotDispatched('user::deleted');
+
+    assertNotSoftDeleted('users', ['id' => $forDeletion->id]);
 });
